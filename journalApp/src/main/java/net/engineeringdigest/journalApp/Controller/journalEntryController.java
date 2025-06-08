eeacff7282feef6805196c822f955f3544b2174f
Ayush <prakashayush414@gmail.com>
2025-06-08 16:28:37 +0530

    two end point is ready

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journalEntryController.java
@@ -1,10 +1,28 @@
 package net.engineeringdigest.journalApp.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.engineeringdigest.journalApp.Entity.journalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping ("/journal")
 public class journalEntryController {
+    private Map<Long, journalEntry>listOfEntries=new HashMap<>();
+    @GetMapping
+    public List<journalEntry> getall()
+    {
+        return new ArrayList<journalEntry>(listOfEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody journalEntry currentEntry)
+    {
+        listOfEntries.put(currentEntry.getId(),currentEntry);
+        return true;
+    }
 
 }
