fc9103b95248c46395850ce0f7d29e0cd56fd53d
Ayush <prakashayush414@gmail.com>
2025-06-08 16:01:43 +0530

    First commit

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Controller/journalEntryController.java
@@ -0,0 +1,10 @@
+package net.engineeringdigest.journalApp.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/journal")
+public class journalEntryController {
+
+}
