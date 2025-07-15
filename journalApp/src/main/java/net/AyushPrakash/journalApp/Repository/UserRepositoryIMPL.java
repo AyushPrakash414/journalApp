254424fb85de8f0c5dea0bba4860167f25413a67
Ayush <prakashayush414@gmail.com>
2025-07-15 20:42:30 +0530

    Exploring the Creteria Api

---
+++ journalApp/src/main/java/net/AyushPrakash/journalApp/Repository/UserRepositoryIMPL.java
@@ -0,0 +1,26 @@
+package net.AyushPrakash.journalApp.Repository;
+
+import net.AyushPrakash.journalApp.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("check")
+public class UserRepositoryIMPL {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @GetMapping
+    public List<User> getUser()
+    {
+        Query query=new Query();
+        query.addCriteria(Criteria.where("userName").is("Ayush"));
+        return mongoTemplate.find(query,User.class);
+    }
+
+}
